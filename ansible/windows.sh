ae471c137bcd137e7db7664075ea8aba05bdd590
Victor Gryshko <vgryshko@brightgrove.com>
2016-01-11 02:04:50 +0200

    Vagrant configuration added

---
+++ ansible/windows.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Update Repositories
+
+# Add Ansible Repository & Install Ansible
+sudo add-apt-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible software-properties-common
+
+# Setup Ansible for Local Use and Run
+cp /vagrant/ansible/inventories/dev /etc/ansible/hosts -f
+chmod 666 /etc/ansible/hosts
+cat /vagrant/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
+sudo ansible-playbook /vagrant/ansible/playbook.yml -e hostname=$1 --connection=local -v
