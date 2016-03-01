e94213399dbb77e332b270ac75347cdeb0708343
Victor Gryshko <vgryshko@brightgrove.com>
2016-03-02 00:37:28 +0200

    Refactored ansible files to use 3rd-party roles

--- ansible/windows.sh
+++ ansible/windows.sh
@@ -5,10 +5,14 @@
 # Add Ansible Repository & Install Ansible
 sudo add-apt-repository -y ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install -y ansible software-properties-common
+sudo apt-get install -y software-properties-common ansible git
 
 # Setup Ansible for Local Use and Run
 cp /vagrant/ansible/inventories/dev /etc/ansible/hosts -f
 chmod 666 /etc/ansible/hosts
 cat /vagrant/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
+
+#Galaxy roles installation
+sudo ansible-galaxy install -r /vagrant/ansible/install_roles.yml
+
 sudo ansible-playbook /vagrant/ansible/playbook.yml -e hostname=$1 --connection=local -v
